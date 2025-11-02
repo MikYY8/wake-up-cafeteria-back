dff194c7057a526eef414e09d5f6e31c846e1125
Micaela Ybarra <mica.yaz03@gmail.com>
2025-11-02 19:19:04 -0300

    Todas las rutas agregadas (por ahora)

--- router/productsRouter.js
+++ router/productsRouter.js
@@ -1,50 +1,34 @@
+// responsable del ruteo (funciones del controller) + ejecutar middlewares
+
 import { Router } from "express";
+import { getOneProduct, getAllProducts, createProduct, updateProduct, updatePartialProduct, deleteProduct } from "../controllers/productsController.js";
 
 const productsRouter = Router()
 
 // CRUD (tiene que tener todos los metodos)
 
 //GET con un :id que va a traer un elemento
-productsRouter.get("/:id", (req, res)=>{
-    // llamar al controller
-
-})
+productsRouter.get("/:id", getOneProduct)
 
 
 //GET con un :id que va a traer todos los elementos (despues lo vamos a controlar)
-productsRouter.get("/", (req, res)=>{
-    // llamar al controller
-    
-})
+productsRouter.get("/", getAllProducts)
 
 
 //POST datos en el body: crea un elemento
-productsRouter.post("/", (req, res)=>{
-    // llamar al controller
-    
-})
+productsRouter.post("/", createProduct)
 
 
 //PUT (editar) datos en el body + id para saber cuál editar: actualiza un elemento especifico
-productsRouter.put("/:id", (req, res)=>{
-    // llamar al controller
-    
-})
+productsRouter.put("/:id", updateProduct)
 
 
 //PATCH datos en el body + id: actualiza un elemento especifico parcialmente
-productsRouter.patch("/:id", (req, res)=>{
-    // llamar al controller
-    
-})
+productsRouter.patch("/:id", updatePartialProduct)
 
 
 //DELETE con un id: eliminar un elemento de la base de datos (logica (cambio de estado) / fisica (eliminacion real de la BBDD))
-productsRouter.delete("/:id", (req, res)=>{
-    // llamar al controller
-    
-})
-
+productsRouter.delete("/:id", deleteProduct)
 
 
 export default productsRouter
