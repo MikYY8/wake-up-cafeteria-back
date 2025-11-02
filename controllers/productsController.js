5cf5318024b13497c069f1b1266e0918de3d88bf
Micaela Ybarra <mica.yaz03@gmail.com>
2025-11-02 19:14:51 -0300

    Controller de productos terminado (por ahora)

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,31 +1,77 @@
 // responsable de quitar las variables de la petición y preparar la salida
 
-export const getOneProduct = (req, res)=>{
-    // logica
+import { productsService } from "../services/productsService.js"
 
-}
+const ps = new productsService();
+
+export const getOneProduct = (req, res)=>{
+    try {
+        // extraer del req las variables
+        const { id } = req.params
+        // llamar al service que resuelve la logica
+        const producto = ps.getOne(id)
+        // devolver resultado
+        res.send(producto)
+    } catch (error) {
+        // devolver error 500: error de servidor
+        res.send(500)
+    }
+
+};
 
 export const getAllProducts = (req, res)=>{
-    // logica
+    try {
+        const productos = ps.getAll()
+        res.send(productos)
+    } catch (error) {
+        
+    }
 
-}
+};
 
 export const createProduct = (req, res)=>{
-    // logica
-
-}
+    const {name, price} = req.body
+    const producto = {
+        name,
+        price
+    }
+    const productoCreado = ps.create(producto)
+    res.send(productoCreado)
+};
 
 export const updateProduct = (req, res)=>{
-    // logica
+    const {id} = req.params
+    const {name,price,status} = req.body
 
-}
+    const producto = {
+        name,price,status,id
+    }
+
+    const productoActualizado = ps.update(producto)
+    res.send(productoActualizado)
+};
 
 export const updatePartialProduct = (req, res)=>{
-    // logica
+    const {id} = req.params
+    const {name,price,status} = req.body
 
-}
+    const producto = { id: Number(id) };
+    if (name !== undefined) producto.name = name;
+    if (price !== undefined) producto.price = price;
+    if (status !== undefined) producto.status = status;
+
+    const productoActualizado = ps.updatePartial(producto)
+    res.send(productoActualizado)
+
+};
 
 export const deleteProduct = (req, res)=>{
-    // logica
+    try {
+        const { id } = req.params
+        const productoEliminado = ps.deleteProd(Number(id))
+        res.send(productoEliminado)
+    } catch (error) {
+        
+    }
 
 }
