f43a18d4535ad6adeaed9483371f5fc9658426a7
Micaela Ybarra <mica.yaz03@gmail.com>
2025-11-02 16:50:33 -0300

    Añado controller de products

---
+++ controllers/productsController.js
@@ -0,0 +1,31 @@
+// responsable de quitar las variables de la petición y preparar la salida
+
+export const getOneProduct = (req, res)=>{
+    // logica
+
+}
+
+export const getAllProducts = (req, res)=>{
+    // logica
+
+}
+
+export const createProduct = (req, res)=>{
+    // logica
+
+}
+
+export const updateProduct = (req, res)=>{
+    // logica
+
+}
+
+export const updatePartialProduct = (req, res)=>{
+    // logica
+
+}
+
+export const deleteProduct = (req, res)=>{
+    // logica
+
+}
