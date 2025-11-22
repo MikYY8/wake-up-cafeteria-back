01f36913e0bec47287ea1fa9853112a5ef4debe2
Micaela Ybarra <mica.yaz03@gmail.com>
2025-11-22 00:44:42 -0300

    services en el directorio correcto

---
+++ src/services/productsService.js
@@ -0,0 +1,47 @@
+import Product from "../models/productsModel.js";
+
+export class productsService{
+
+    async getOne(id){
+        const producto = await Product.findOne({id:Number(id)})
+        return producto
+    }
+
+    async getAll(){
+        const products = await Product.find()
+        return products
+    }
+
+    async create(name,price){
+
+        const idGenerado = Math.ceil((Math.random()*1000000)+1)
+
+        const producto = {
+            name,
+            price,
+            status:true,
+            id:idGenerado
+        }
+        const productoCreado = await Product.create(producto)
+        return productoCreado
+    }
+
+    async update(producto){
+        const productoActualizado = await Product.updateOne({id:producto.id},{...producto})
+        return productoActualizado
+    }
+
+    async deleteLogicoProd(id){
+        //  borrado logico
+        const productoEliminado = await Product.updateOne({id:id},{status:false})
+        return productoEliminado
+
+    }
+
+    // async deleteFisicoProd(id){
+    //     //   borrado fisico
+    //     const productoEliminado = await Product.deleteOne({id:id})
+    //     return productoEliminado
+    // }
+
+} 
