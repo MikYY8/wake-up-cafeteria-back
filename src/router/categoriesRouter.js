60a0547171366c168f29fef3390353ac0830149b
Micaela Ybarra <mica.yaz03@gmail.com>
2025-11-22 00:43:30 -0300

    añado validaciones en las rutas

---
+++ src/router/categoriesRouter.js
@@ -0,0 +1,30 @@
+import { Router } from "express";
+import { getOneCategory, getAllCategories, createCategory, updateCategory, deleteCategory } from "../controllers/categoriesController.js";
+import { idValidation, categoryCreateValidation, categoryUpdateValidation } from "../validations/categoriesValidation.js"
+import { validationMiddleware } from "../middlewares/validationMiddleware.js";
+
+const categoriesRouter = Router()
+
+// CRUD (tiene que tener todos los metodos)
+
+//GET con un :id que va a traer un elemento
+categoriesRouter.get("/:id", idValidation, validationMiddleware, getOneCategory)
+
+
+//GET con un :id que va a traer todos los elementos (despues lo vamos a controlar)
+categoriesRouter.get("/", getAllCategories)  
+
+
+//POST datos en el body: crea un elemento
+categoriesRouter.post("/", categoryCreateValidation, validationMiddleware, createCategory)
+
+
+//PUT (editar) datos en el body + id para saber cuál editar: actualiza un elemento especifico
+categoriesRouter.put("/:id", categoryUpdateValidation, validationMiddleware, updateCategory)
+
+
+//DELETE con un id: eliminar un elemento de la base de datos (logica (cambio de estado) / fisica (eliminacion real de la BBDD))
+categoriesRouter.delete("/:id", idValidation, validationMiddleware, deleteCategory)
+
+
+export default categoriesRouter
